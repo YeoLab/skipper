7b4d99d66761fc361754bd2b62cea5afe335d8a3
Shuhao Xu <s5xu@tscc-9-16.tscc.sdsc.edu>
2025-05-14 15:47:50 -0700

    update

--- rules/ctk_mcross.py
+++ rules/ctk_mcross.py
@@ -17,17 +17,15 @@ data_types = ['CIMS', 'CITS']
 
 rule all:
     input:
-        bam_informative=f"{OUTPUT}/bams/genome_R1/{replicate_label}.genome.Aligned.sort.dedup.R1.bam",
-        finemapped_fa = f"{OUTPUT}/ml/sequence/{experiment_label}.foreground.fa",
-        background_fa = f"{OUTPUT}/ml/sequence/{experiment_label}.background.fa",
-        bam_umap = f"{OUTPUT}/bams/genome/{replicate_label}.genome.Aligned.sort.dedup.umap.bam",
-        bai_umap = f"{OUTPUT}/bams/genome/{replicate_label}.genome.Aligned.sort.dedup.umap.bam.bai",
-        mdtag = f"{OUTPUT}/bams/genome/{replicate_label}.genome.Aligned.sort.dedup.umap.md.sam",
-        mutation_file = f"{OUTPUT}/ctk/{replicate_label}.mutation.txt",
-        tagbed = f"{OUTPUT}/ctk/{replicate_label}.tag.bed",
-        peak = f"{OUTPUT}/ctk/{replicate_label}.uniq.peak.sig.bed",
-        peak_bd = f"{OUTPUT}/ctk/{replicate_label}.uniq.peak.sig.boundary.bed",
-        peak_PH = f"{OUTPUT}/ctk/{replicate_label}.uniq.peak.sig.halfPH.bed",
+        bam_informative = expand(os.path.join(OUTPUT, "bams/genome_R1/{replicate_label}.genome.Aligned.sort.dedup.R1.bam"), replicate_label=replicate_labels),
+        bam_umap = expand(os.path.join(OUTPUT, "bams/genome/{replicate_label}.genome.Aligned.sort.dedup.umap.bam"), replicate_label=replicate_labels),
+        bai_umap = expand(os.path.join(OUTPUT, "bams/genome/{replicate_label}.genome.Aligned.sort.dedup.umap.bam.bai"), replicate_label=replicate_labels),
+        mdtag = expand(os.path.join(OUTPUT, "bams/genome/{replicate_label}.genome.Aligned.sort.dedup.umap.md.sam"), replicate_label=replicate_labels),
+        mutation_file = expand(os.path.join(OUTPUT, "ctk/{replicate_label}.mutation.txt"), replicate_label=replicate_labels),
+        tagbed = expand(os.path.join(OUTPUT, "ctk/{replicate_label}.tag.bed"), replicate_label=replicate_labels),
+        peak = expand(os.path.join(OUTPUT, "ctk/{replicate_label}.uniq.peak.sig.bed"), replicate_label=replicate_labels),
+        peak_bd = expand(os.path.join(OUTPUT, "ctk/{replicate_label}.uniq.peak.sig.boundary.bed"), replicate_label=replicate_labels),
+        peak_PH = expand(os.path.join(OUTPUT, "ctk/{replicate_label}.uniq.peak.sig.halfPH.bed"), replicate_label=replicate_labels),
         mutation_beds = expand(os.path.join(OUTPUT, "ctk", "{replicate_label}.uniq.{mutation_type}.bed"), replicate_label=replicate_labels, mutation_type=mutation_types),
         mutation_CIMSs = expand(os.path.join(OUTPUT, "ctk/CIMS/{replicate_label}.uniq.{mutation_type}.CIMS.txt"), replicate_label=replicate_labels, mutation_type=mutation_types),
         mutation_CIMS_stats = expand(os.path.join(OUTPUT, "ctk/CIMS/{replicate_label}.uniq.{mutation_type}.pos.stat.CIMS.txt"), replicate_label=replicate_labels, mutation_type=mutation_types),
@@ -47,41 +45,41 @@ rule all:
         top_peak = expand(os.path.join(OUTPUT, "ctk/mcross/top7mer/{data_type}.{replicate_label}.{mutation_type}.top.txt"), data_type=data_types, replicate_label=replicate_labels, mutation_type=mutation_types),
 
         
-rule fetch_sequence:
-    input:
-        finemapped_windows = f"{OUTPUT}/finemapping/mapped_sites/{experiment_label}.finemapped_windows.bed.gz",
-        background = f"{OUTPUT}/homer/region_matched_background/fixed/{experiment_label}.sampled_fixed_windows.bed.gz",
-    output:
-        finemapped_fa = f"{OUTPUT}/ml/sequence/{experiment_label}.foreground.fa",
-        background_fa = f"{OUTPUT}/ml/sequence/{experiment_label}.background.fa"
-    params:
-        error_out_file = f"{OUTPUT}/stderr/{experiment_label}.fetch_sequence.err",
-        out_file = f"{OUTPUT}/stdout/{experiment_label}.fetch_sequence.out",
-        run_time = "40:00",
-        memory = "2000",
-        job_name = "run_homer",
-        fa = "/tscc/projects/ps-yeolab4/genomes/GRCh38/chromosomes/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta",
-    container:
-        "docker://howardxu520/skipper:samtools_1.17_bedtools_2.31.0"
-    shell:
-        '''
-        bedtools getfasta -fo {output.finemapped_fa} -fi {params.fa} -bed {input.finemapped_windows} -s
-        bedtools getfasta -fo {output.background_fa} -fi {params.fa} -bed {input.background} -s
-        '''
+# rule fetch_sequence:
+#     input:
+#         finemapped_windows = f"{OUTPUT}/finemapping/mapped_sites/{experiment_label}.finemapped_windows.bed.gz",
+#         background = f"{OUTPUT}/homer/region_matched_background/fixed/{experiment_label}.sampled_fixed_windows.bed.gz",
+#     output:
+#         finemapped_fa = f"{OUTPUT}/ml/sequence/{experiment_label}.foreground.fa",
+#         background_fa = f"{OUTPUT}/ml/sequence/{experiment_label}.background.fa"
+#     params:
+#         error_out_file = f"{OUTPUT}/stderr/{experiment_label}.fetch_sequence.err",
+#         out_file = f"{OUTPUT}/stdout/{experiment_label}.fetch_sequence.out",
+#         run_time = "40:00",
+#         memory = "2000",
+#         job_name = "run_homer",
+#         fa = "/tscc/projects/ps-yeolab4/genomes/GRCh38/chromosomes/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta",
+#     container:
+#         "docker://howardxu520/skipper:samtools_1.17_bedtools_2.31.0"
+#     shell:
+#         '''
+#         bedtools getfasta -fo {output.finemapped_fa} -fi {params.fa} -bed {input.finemapped_windows} -s
+#         bedtools getfasta -fo {output.background_fa} -fi {params.fa} -bed {input.background} -s
+#         '''
         
 
 rule select_informative_read:
     input:
-        bam_combined=f"{OUTPUT}/bams/genome/{replicate_label}.genome.Aligned.sort.dedup.bam"
+        bam_combined=os.path.join(OUTPUT, "bams/genome/{replicate_labels}.genome.Aligned.sort.dedup.bam"),
     output:
-        bam_informative=f"{OUTPUT}/bams/genome_R1/{replicate_label}.genome.Aligned.sort.dedup.R1.bam"
+        bam_informative=os.path.join(OUTPUT, "bams/genome_R1/{replicate_labels}.genome.Aligned.sort.dedup.R1.bam"),
     params:
-        error_out_file = f"{OUTPUT}/stderr/{replicate_label}.select_informative_read.err",
-        out_file = f"{OUTPUT}/stdout/{replicate_label}.select_informative_read.out",
+        error_out_file = os.path.join(OUTPUT, "stderr/{replicate_labels}.select_informative_read.err"),
+        out_file = os.path.join(OUTPUT, "stdout/{replicate_labels}.select_informative_read.out"),
         run_time = "00:30:00",
         memory = "10000",
-        job_name = "select_informative_read",
-    benchmark: f"{OUTPUT}/benchmarks/select/unassigned_experiment.{replicate_label}.select_informative_read.txt"
+        job_name = "select_informative_read {replicate_labels}",
+    benchmark: os.path.join(OUTPUT, "benchmarks/select/unassigned_experiment.{replicate_labels}.select_informative_read.txt"),
     container:
         "docker://howardxu520/skipper:samtools_1.17_bedtools_2.31.0"
     threads: 8
@@ -93,15 +91,15 @@ rule uniquely_mapped_reads:
     input:
         bam = rules.select_informative_read.output.bam_informative
     output:
-        bam_umap = f"{OUTPUT}/bams/genome/{replicate_label}.genome.Aligned.sort.dedup.umap.bam",
-        bai_umap = f"{OUTPUT}/bams/genome/{replicate_label}.genome.Aligned.sort.dedup.umap.bam.bai",
+        bam_umap = os.path.join(OUTPUT, "bams/genome/{replicate_labels}.genome.Aligned.sort.dedup.umap.bam"),
+        bai_umap = os.path.join(OUTPUT, "bams/genome/{replicate_labels}.genome.Aligned.sort.dedup.umap.bam.bai"),
     threads: 1
     params:
-        error_out_file = f"{OUTPUT}/stderr/uniquemap.{replicate_label}.err",
-        out_file = f"{OUTPUT}/stdout/uniquemap.{replicate_label}.out",
+        error_out_file = os.path.join(OUTPUT, "stderr/uniquemap.{replicate_labels}.err"),
+        out_file = os.path.join(OUTPUT, "stdout/uniquemap.{replicate_labels}.out"),
         run_time = "0:30:00",
         memory = 40000,
-        job_name = "filter_map_quality",
+        job_name = "filter_map_quality {replicate_labels}",
     threads: 8
     shell:
         """
@@ -112,17 +110,17 @@ rule uniquely_mapped_reads:
         
 rule md_tag:
     input:
-        bam_ip_umap = f"{OUTPUT}/bams/genome/{replicate_label}.genome.Aligned.sort.dedup.umap.bam"
+        bam_ip_umap = os.path.join(OUTPUT, "bams/genome/{replicate_labels}.genome.Aligned.sort.dedup.umap.bam"),
     output:
-        mdtag = f"{OUTPUT}/bams/genome/{replicate_label}.genome.Aligned.sort.dedup.umap.md.sam",
+        mdtag = os.path.join(OUTPUT, "bams/genome/{replicate_labels}.genome.Aligned.sort.dedup.umap.md.sam"),
     params:
         fa = "/tscc/projects/ps-yeolab4/genomes/GRCh38/chromosomes/GRCh38_no_alt_analysis_set_GCA_000001405.15.fasta",
-        error_out_file = f"{OUTPUT}/error_files/md_tag.{replicate_label}.err",
-        out_file = f"{OUTPUT}/stdout/md_tag.{replicate_label}.out",
+        error_out_file = os.path.join(OUTPUT, "error_files/md_tag.{replicate_labels}.err"),
+        out_file = os.path.join(OUTPUT, "stdout/md_tag.{replicate_labels}.out"),
         run_time = "1:00:00",
         memory = 5000,
         cores = 1,
-        job_name = "fillmd",
+        job_name = "fillmd {replicate_labels}",
     threads: 8
     container:
         "docker://howardxu520/skipper:samtools_1.17_bedtools_2.31.0"
@@ -134,20 +132,20 @@ rule md_tag:
     
 rule ctk_parse:
     input:
-        mdtag = f"{OUTPUT}/bams/genome/{replicate_label}.genome.Aligned.sort.dedup.umap.md.sam",
+        mdtag = os.path.join(OUTPUT, "bams/genome/{replicate_labels}.genome.Aligned.sort.dedup.umap.md.sam"),
     output:
-        mutation_file = f"{OUTPUT}/ctk/{replicate_label}.mutation.txt",
-        tagbed = f"{OUTPUT}/ctk/{replicate_label}.tag.bed",
-        peak = f"{OUTPUT}/ctk/{replicate_label}.uniq.peak.sig.bed",
-        peak_bd = f"{OUTPUT}/ctk/{replicate_label}.uniq.peak.sig.boundary.bed",
-        peak_PH = f"{OUTPUT}/ctk/{replicate_label}.uniq.peak.sig.halfPH.bed",
+        mutation_file = os.path.join(OUTPUT, "ctk/{replicate_labels}.mutation.txt"),
+        tagbed = os.path.join(OUTPUT, "ctk/{replicate_labels}.tag.bed"),
+        peak = os.path.join(OUTPUT, "ctk/{replicate_labels}.uniq.peak.sig.bed"),
+        peak_bd = os.path.join(OUTPUT, "ctk/{replicate_labels}.uniq.peak.sig.boundary.bed"),
+        peak_PH = os.path.join(OUTPUT, "ctk/{replicate_labels}.uniq.peak.sig.halfPH.bed"),
     params:
-        error_out_file = f"{OUTPUT}/error_files/ctk_parse.{replicate_label}.err",
-        out_file = f"{OUTPUT}/stdout/ctk_parse.{replicate_label}.out",
+        error_out_file = os.path.join(OUTPUT, "error_files/ctk_parse.{replicate_labels}.err"),
+        out_file = os.path.join(OUTPUT, "stdout/ctk_parse.{replicate_labels}.out"),
         run_time = "2:00:00",
         memory = 10000,
         cores = 1,
-        job_name = "parse",
+        job_name = "parse {replicate_labels}",
     threads: 8
     # container:
     #     "docker://howardxu520/skipper:samtools_1.17_bedtools_2.31.0"
@@ -201,15 +199,16 @@ rule ctk_CIMS:
     params:
         error_out_file = os.path.join(OUTPUT, "error_files/ctk_CIMS.{replicate_labels}.{mutation_types}.err"),
         out_file = os.path.join(OUTPUT, "stdout/ctk_CIMS.{replicate_labels}.{mutation_types}.out"),
-        run_time = "2:00:00",
-        memory = 10000,
+        run_time = "3:00:00",
+        memory = 100000,
         cores = 1,
         job_name = "CIMS {replicate_labels} {mutation_types}",
+        tpm_dir = os.path.join(OUTPUT, "tpm", "CIMS.{replicate_labels}.{mutation_types}"),
     # container:
     #     "docker://howardxu520/skipper:samtools_1.17_bedtools_2.31.0"
     shell:
         """
-        perl /tscc/nfs/home/s5xu/projects/ctk-1.1.5/CIMS.pl -big -n 10 -p -outp {output.mutation_CIMS_stat} -v {input.tagbed} {input.mutation} {output.mutation_CIMS}
+        perl /tscc/nfs/home/s5xu/projects/ctk-1.1.5/CIMS.pl -big -n 10 -p -c {params.tpm_dir} -outp {output.mutation_CIMS_stat} -v {input.tagbed} {input.mutation} {output.mutation_CIMS}
         """
         
 rule ctk_CIMS_process:
@@ -245,7 +244,7 @@ rule fetch_CIMS_sequence:
         finemapped_CIMS_fa = os.path.join(OUTPUT, "sequence/CIMS.{replicate_labels}.{mutation_types}.foreground.fa"),
         background_CIMS_fa = os.path.join(OUTPUT, "sequence/CIMS.{replicate_labels}.{mutation_types}.background.fa"),
     params:
-        error_file = os.path.join(OUTPUT, "stderr/CIMS.{replicate_labels}.{mutation_types}.fetch_sequence.err"),
+        error_out_file = os.path.join(OUTPUT, "stderr/CIMS.{replicate_labels}.{mutation_types}.fetch_sequence.err"),
         out_file = os.path.join(OUTPUT, "stdout/CIMS.{replicate_labels}.{mutation_types}.fetch_sequence.out"),
         run_time = "30:00",
         memory = "5000",
@@ -284,12 +283,13 @@ rule ctk_CITS:
         memory = 10000,
         cores = 1,
         job_name = "CITS {replicate_labels} {mutation_types}",
+        tpm_dir = os.path.join(OUTPUT, "tpm", "CITS.{replicate_labels}.{mutation_types}"),
     threads: 8
     # container:
     #     "docker://howardxu520/skipper:samtools_1.17_bedtools_2.31.0"
     shell:
         """
-        perl /tscc/nfs/home/s5xu/projects/ctk-1.1.5/CITS.pl -big -p 0.001 --gap 25 -v {input.tagbed} {input.mutation} {output.mutation_CITS}
+        perl /tscc/nfs/home/s5xu/projects/ctk-1.1.5/CITS.pl -big -p 0.001 --gap 25 -c {params.tpm_dir} -v {input.tagbed} {input.mutation} {output.mutation_CITS}
         """   
                        
         
@@ -324,7 +324,7 @@ rule fetch_CITS_sequence:
         finemapped_CITS_fa = os.path.join(OUTPUT, "sequence/CITS.{replicate_labels}.{mutation_types}.foreground.fa"),
         background_CITS_fa = os.path.join(OUTPUT, "sequence/CITS.{replicate_labels}.{mutation_types}.background.fa"),
     params:
-        error_file = os.path.join(OUTPUT, "stderr/CITS.{replicate_labels}.{mutation_types}.fetch_sequence.err"),
+        error_out_file = os.path.join(OUTPUT, "stderr/CITS.{replicate_labels}.{mutation_types}.fetch_sequence.err"),
         out_file = os.path.join(OUTPUT, "stdout/CITS.{replicate_labels}.{mutation_types}.fetch_sequence.out"),
         run_time = "30:00",
         memory = "5000",
@@ -359,7 +359,7 @@ rule mcross_get_kmer_seed:
         topn_kmer_matrix = os.path.join(OUTPUT, "ctk/mcross/{data_types}.{replicate_labels}.{mutation_types}.w7.zcore.mat.txt"),
         top_peak = os.path.join(OUTPUT, "ctk/mcross/top7mer/{data_types}.{replicate_labels}.{mutation_types}.top.txt"),
     params:
-        error_file = os.path.join(OUTPUT, "error_files/{data_types}.{replicate_labels}.{mutation_types}.mcross_kmer.err"),
+        error_out_file = os.path.join(OUTPUT, "error_files/{data_types}.{replicate_labels}.{mutation_types}.mcross_kmer.err"),
         out_file = os.path.join(OUTPUT, "stdout/{data_types}.{replicate_labels}.{mutation_types}.mcross_kmer.out"),
         run_time = "2:00:00",
         memory = 10000,
